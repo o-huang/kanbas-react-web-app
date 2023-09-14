af8d755bca54e463206fd37e7089c5f71c398324
Owen Huang <ohuang2@gmail.com>
2023-09-14 18:27:56 -0400

    hw1 initial push

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <a href='/kanbas/main.html'>Kanbas</a>
+        <a href='/labs/a1/index.html'>Lab</a>
       </header>
     </div>
   );
