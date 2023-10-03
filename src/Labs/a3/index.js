2c07d015567f0ccda9b8e9ed259bb5303401d9ac
Owen Huang <ohuang2@gmail.com>
2023-10-03 15:13:50 -0400

    lab 3

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,9 +1,19 @@
 import JavaScript from "./JavaScript";
+
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./todo/TodoList";
 function Assignment3() {
  return (
    <div>
      <h1>Assignment 3</h1>
+     <TodoList/>
+     <ConditionalOutput/>
+     <Styles/>
+     <Classes/>
      <JavaScript/>
+
    </div>
  );
 }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
  <React.StrictMode>
    <App />
+   
  </React.StrictMode>
+ 
 );
