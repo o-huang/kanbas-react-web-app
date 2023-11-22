d59b4c22cd4e69da1ccfe40f71b1572c7543a489
Owen Huang <ohuang2@gmail.com>
2023-11-22 00:46:36 -0500

    change api_base

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -3,9 +3,10 @@ import axios from "axios";
 const request = axios.create({
   withCredentials: true,
 });
-
-export const BASE_API = process.env.REACT_APP_BASE_API_URL;
-export const USERS_API = `${BASE_API}/api/users`;
+const API_BASE = process.env.REACT_APP_API_BASE;
+// export const BASE_API = process.env.REACT_APP_BASE_API_URL;
+export const USERS_API = `${API_BASE}/users`;
+console.log(USERS_API);
 export const signin = async (credentials) => {
   const response = await request.post(`${USERS_API}/signin`, credentials);
   return response.data;
